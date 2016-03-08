6bd5ccd936f176663f6c7702e84bc96752aacbfc
mnyeks <0719075643kennyg@gmail.com>
2016-03-08 13:15:05 +0200

    Best deal Kata

---
+++ avo.js
@@ -0,0 +1,15 @@
+function findTheString() {
+     
+    var avoStr = ["1 for R3, 2 for R7, 3 for R10, 5 for R14.50."];
+
+    
+         var string = avoStr.split(",");
+
+
+     var avoList = [];
+     
+     //for(var i = 0; i < findTheString.length; i++) {
+         //var bestPlace = findTheString[i];
+       
+              };     
+ 
---
+++ test_js.js
@@ -0,0 +1,10 @@
+
+
+QUnit.test("findTheString",function(assert) {
+   var fhp = findTheString("convert the string into an array (split out the quantity and the price)");
+assert.equal("1 for R3, 2 for R7, 3 for R10, 5 for R14.50", findTheString);
+});
+
+//QUnit.test("", function(assert) {
+  //var
+//});
